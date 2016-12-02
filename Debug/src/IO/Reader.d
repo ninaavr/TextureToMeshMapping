5ae5e205c49b5443121adccdd6d54e878d99a65f
ninaavr <nina@bssbg.com>
2016-12-02 03:34:54 +0100

    free vertices of conformal map loaded in matrix, code improved

--- Debug/src/IO/Reader.d
+++
@@ -1,9 +0,0 @@
-src/IO/Reader.d: ../src/IO/Reader.cpp ../src/IO/Reader.h \
- ../src/IO/../Textured_polyhedron.h \
- ../src/IO/../Textured_polyhedron_builder.h
-
-../src/IO/Reader.h:
-
-../src/IO/../Textured_polyhedron.h:
-
-../src/IO/../Textured_polyhedron_builder.h:
--- Debug/src/IO/Writer.d
+++
@@ -1,6 +0,0 @@
-src/IO/Writer.d: ../src/IO/Writer.cpp ../src/IO/Writer.h \
- ../src/IO/../Textured_polyhedron.h
-
-../src/IO/Writer.h:
-
-../src/IO/../Textured_polyhedron.h:
